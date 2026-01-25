8c4ad9a84dddfef559d6355a5f0e74ed1ea1f857
Jhziiiig <149804421+Jhziiiig@users.noreply.github.com>
2026-01-25 08:39:14 +0800

    Update dataset_gen.sh

--- dataset_gen.sh
+++ dataset_gen.sh
@@ -1,6 +1,6 @@
 
-python DataGeneration.py --datasets milan --threshold 0.01 --maxlen 1500
-python DataGeneration.py --datasets cairo --threshold 0.01 --maxlen 1500 
-python DataGeneration.py --datasets kyoto7 --threshold 0.1 --maxlen 1500 
+python DataGeneration.py --datasets milan --threshold 0.01 --maxlen 1500 --bin 1
+python DataGeneration.py --datasets cairo --threshold 0.01 --maxlen 1500 --bin 1
+python DataGeneration.py --datasets kyoto7 --threshold 0.1 --maxlen 1500 --bin 1
 
 
