eabc259394f57c3972034ed7828f9c62fcb4b4de
Jhziiiig <149804421+Jhziiiig@users.noreply.github.com>
2026-01-25 08:34:39 +0800

    Update dataset_gen.sh

--- dataset_gen.sh
+++ dataset_gen.sh
@@ -1,8 +1,6 @@
 
-# python DataGeneration.py --datasets orange --mode sensor --threshold 0.00
-# python DataGeneration.py --datasets milan  --mode sensor --maxlen 1500 --threshold 0.01 --off
-# python DataGeneration.py --datasets cairo  --mode sensor --threshold 0.01 --maxlen 1500 --off
-python DataGeneration.py --datasets kyoto7  --mode sensor --threshold 0.1 --maxlen 1500 --off
-
+python DataGeneration.py --datasets milan --threshold 0.01 --maxlen 1500
+python DataGeneration.py --datasets cairo --threshold 0.01 --maxlen 1500 
+python DataGeneration.py --datasets kyoto7 --threshold 0.1 --maxlen 1500 
 
 
